e5231fb553c2e19b2d51bc445d8c3da495fa77a6
Ramen5201 <dltjsdn1001@naver.com>
2023-10-05 15:36:57 +0900

    init

---
+++ JavaScript/basic/12.callback_and_promise.js
@@ -0,0 +1,22 @@
+/**
+ * callback
+ */
+
+// function getHuman(callback){
+
+//     setTimeout(function(){ //서버에 요청한다는 가정(응답은 2초가 걸린다.)
+//         let human = {
+//             name : "김시원",
+//             age: 44
+//         }
+//         callback(human)
+//     },2000)
+// }
+
+// let retrunFunk = function(human){
+//     console.log('응답완료')
+//     console.log(human.name)
+//     console.log(human.age)
+// }
+
+// console.log('요청전송')
